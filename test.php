8a9df2ccbec26a7f87d6956474b4fee90440790d
OTS Digital Communication Platform <bkhanfar@otsdc.com>
2015-06-08 11:50:10 +0300

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,6 @@ use \OTS\API\Client;
 $client = new Client();
 
 //Kindly note that the newly added sender ID nust me approved by our system if you have any issue please contact our support team
-//Send bulk needs advance permission please contact our support team to start using it.
 try {
     $response = $client->Account->AddSenderID('Hello');
     $response = $client->Voice->Call('',f);
