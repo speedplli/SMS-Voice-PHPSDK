b710af732db13aa7332b738999781f96ec5d9691
ahmadAlhusainy <arahhal@otsdc.com>
2015-10-20 15:42:06 +0300

    changeunifonic

--- OTS/API/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace OTS\API;
-
-
-class Exception extends \Exception
-{
-
-
-}
--- OTS/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-    return array(
-        'AppSid' => '9_kwDSfe_k3DOcyjO_R40Fke6ubIKB',
-        'ApiURL' => 'http://api.otsdc.com/rest/',
-
-    );
