c2399347ffff386530706e9c65e7fbdd24c810ea
Your Name <you@example.com>
2016-07-13 13:39:55 +0300

    Add Text Massages Examples

--- test.php
+++ test.php
@@ -5,9 +5,16 @@ $client = new Client();
 
 //Kindly note that the newly added sender ID nust me approved by our system if you have any issue please contact our support team
 try {
-    $response = $client->Account->AddSenderID('Hello');
+    //$response = $client->Messages->Send('recipient','messageBody','senderName');
+	$response = $client->Messages->SendBulkMessages('966505050505,966555655555','Hello','UNIFONIC');
+
+    /* Examples */
+    //$response = $client->Account->AddSenderID('newSenderName');
     //$response = $client->Voice->Call('',f);
-    $response = $client->Messages->SendBulkMessages('966505050505,966555655555','Hello','UNIFONIC');
+    //$response = $client->Account->GetBalance();
+    
+    print_r($response);
+     
 } catch (Exception $e) {
 
     echo $e->getCode();
